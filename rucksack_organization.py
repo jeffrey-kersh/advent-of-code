f568f8b59992afcacb20837291d258d1fc73541b
jayii23 <66144667+jayii23@users.noreply.github.com>
2022-12-04 00:26:36 -0800

    Part 1 - Day 3

---
+++ rucksack_organization.py
@@ -0,0 +1,81 @@
+import os, sys
+
+# list priorities
+priority_dict = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+    "d": 4,
+    "e": 5,
+    "f": 6,
+    "g": 7,
+    "h": 8,
+    "i": 9,
+    "j": 10,
+    "k": 11,
+    "l": 12,
+    "m": 13,
+    "n": 14,
+    "o": 15,
+    "p": 16,
+    "q": 17,
+    "r": 18,
+    "s": 19,
+    "t": 20,
+    "u": 21,
+    "v": 22,
+    "w": 23,
+    "x": 24,
+    "y": 25,
+    "z": 26,
+    "A": 27,
+    "B": 28,
+    "C": 29,
+    "D": 30,
+    "E": 31,
+    "F": 32,
+    "G": 33,
+    "H": 34,
+    "I": 35,
+    "J": 36,
+    "K": 37,
+    "L": 38,
+    "M": 39,
+    "N": 40,
+    "O": 41,
+    "P": 42,
+    "Q": 43,
+    "R": 44,
+    "S": 45,
+    "T": 46,
+    "U": 47,
+    "V": 48,
+    "W": 49,
+    "X": 50,
+    "Y": 51,
+    "Z": 52
+}
+
+# read raw data into list
+raw_data_path = os.path.join(sys.path[0], 'day_3_input.txt')
+with open(raw_data_path) as raw_data:
+    rucksack_data = raw_data.readlines()
+
+priorities_sum = 0
+
+for rucksack in rucksack_data:
+    rucksack = rucksack.strip()
+    mid = int(len(rucksack)/2)
+    end = len(rucksack)
+    front_compartment = rucksack[0:mid]
+    back_compartment = rucksack[mid:end]
+    for f_item in front_compartment:
+        for b_item in back_compartment:
+            if f_item == b_item:
+                priority = priority_dict[f_item]
+                priorities_sum = priorities_sum + priority
+                break
+        if f_item == b_item:
+            break
+
+print(priorities_sum)
